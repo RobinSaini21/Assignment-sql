8dc2663292c13a7c8330af03274aba0f4102bc5a
Robin Saini <robinsaini2126@gmail.com>
2024-03-10 22:18:48 +0530

    data inster fixes

--- controllers/createAirCrafts.js
+++ controllers/createAirCrafts.js
@@ -1,26 +1,55 @@
+const MissionSummaries = require("../schema/MissionSummaries");
 
-const MissionSummaries = require("../schema/MissionSummaries")
+const fieldMap = {
+  'Pilot Id': 'pilot_id',
+  'Pilot Name': 'pilot_name',
+  'Drone Code': 'drone_code',
+  'Drone UIN': 'drone_uin',
+  'Drone UUID': 'drone_uuid',
+  'Mission Height': 'mission_height',
+  'Clearance Height': 'clearance_height',
+  'Payload At Start': 'payload_at_start',
+  'Payload At End': 'payload_at_end',
+  'Battery Capacity At Start': 'battery_capacity_at_start',
+  'Battery Capacity At End': 'battery_capacity_at_end',
+  'Area Sprayed At Start': 'area_sprayed_at_start',
+  'Area Sprayed At End': 'area_sprayed_at_end',
+  'Flight Time': 'flight_time',
+  Boundary: 'boundary',
+  Plan: 'plan',
+  Warnings: 'warnings',
+  'Arm Cycle': 'arm_cycle'
+}
 
-  module.exports = async function creatAirCraft(req, res) {
-    try {
-      const arm_summarys = req.body.arm_summary;
-  
-      await Promise.all(
-        arm_summarys.map(async (parameters = []) => {
-          const mappedParameters = parameters.parameters.map((data) => ({
-            pilot_name: data?.name,
-            units: data?.units,
-            other: data?.value,
-          }));
-  
 
-          await MissionSummaries.bulkCreate(mappedParameters);
-        })
-      );
-  
-      return res.status(200).send({ message: "Bulk User Saved" });
-    } catch (error) {
-      return res.status(500).send({ message: "Internal server error" , error });
-    }
-  };
-  
+module.exports = async function creatAirCraft(req, res) {
+  try {
+    const arm_summarys = req.body.arm_summary;
+
+    await Promise.all(
+      arm_summarys.map(async (parameters = []) => {
+        const mappedParameters = parameters.parameters.map((data) => {
+          if (fieldMap[data?.name]) {
+            return {
+              [fieldMap[data?.name]]: data?.value,
+              units: data?.units
+            };
+          } else {
+            return {
+              units: data?.units,
+              other: data?.value,
+            };
+          }
+        });
+
+        await MissionSummaries.bulkCreate(mappedParameters);
+      })
+    );
+
+    return res.status(200).send({ message: "Bulk User Saved" });
+  } catch (error) {
+    return res.status(500).send({ message: "Internal server error", error });
+  }
+};
+
+
--- controllers/getArmSummarys.js
+++ controllers/getArmSummarys.js
@@ -1,31 +1,88 @@
 const { Op } = require("sequelize");
 const MissionSummaries = require("../schema/MissionSummaries");
 
+
+const fieldMap = {
+  pilot_id: 'Pilot Id',
+  pilot_name: 'Pilot Name',
+  drone_code: 'Drone Code',
+  drone_uin: 'Drone UIN',
+  drone_uuid: 'Drone UUID',
+  mission_height: 'Mission Height',
+  clearance_height: 'Clearance Height',
+  payload_at_start: 'Payload At Start',
+  payload_at_end: 'Payload At End',
+  battery_capacity_at_start: 'Battery Capacity At Start',
+  battery_capacity_at_end: 'Battery Capacity At End',
+  area_sprayed_at_start: 'Area Sprayed At Start',
+  area_sprayed_at_end: 'Area Sprayed At End',
+  flight_time: 'Flight Time',
+  boundary: 'Boundary',
+  plan: 'Plan',
+  warnings: 'Warnings',
+  arm_cycle: 'Arm Cycle',
+
+}
+
 module.exports = async function getArmSummarys(req, res) {
   try {
     const missionSummaries = await MissionSummaries.findAll({
       where: {
         [Op.or]: [
+          { pilot_id: { [Op.not]: null } },
+          { mission_uuid: { [Op.not]: null } },
+          { drone_code: { [Op.not]: null } },
+          { drone_uin: { [Op.not]: null } },
+          { drone_uuid: { [Op.not]: null } },
+          { plan: { [Op.not]: null } },
+          { mission_height: { [Op.not]: null } },
+          { boundary_name: { [Op.not]: null } },
+          { created_at: { [Op.not]: null } },
+          { edited_at: { [Op.not]: null } },
           { pilot_name: { [Op.not]: null } },
-          { units: { [Op.not]: null } },
+          { mission_started_at: { [Op.not]: null } },
+          { mission_ended_at: { [Op.not]: null } },
+          { take_off_location: { [Op.not]: null } },
+          { clearance_height: { [Op.not]: null } },
+          { payload_at_start: { [Op.not]: null } },
+          { payload_at_end: { [Op.not]: null } },
+          { battery_capacity_at_start: { [Op.not]: null } },
+          { battery_capacity_at_end: { [Op.not]: null } },
+          { area_sprayed_at_start: { [Op.not]: null } },
+          { area_sprayed_at_end: { [Op.not]: null } },
+          { flight_time: { [Op.not]: null } },
+          { battery_sn_one: { [Op.not]: null } },
+          { battery_sn_two: { [Op.not]: null } },
+          { arm_cycle: { [Op.not]: null } },
           { other: { [Op.not]: null } },
+          { units: { [Op.not]: null } },
+          { warnings: { [Op.not]: null } },
         ],
-      },
-      attributes: [
-        ["pilot_name", "name"],
-        ["units", "units"],
-        ["other", "value"],
-      ],
+      },      
     });
 
-    missionSummaries.forEach((data) => {
-      try {
-        data.dataValues.value = JSON.parse(data.dataValues.value);
-      } catch (error) {
-        data.dataValues.value = data.dataValues.value;
-      }
+  const reducedData = missionSummaries.map((data) => {
+
+      return Object.entries(data.dataValues).reduce((acc, [currKey, currValue]) => {
+     
+      
+        if ((currValue || typeof currValue === "string") && fieldMap[currKey]) {
+            return { ...acc, name: fieldMap[currKey] , units: data.dataValues.units , value: data.dataValues[currKey]};
+          
+        }
+        if(currKey === "other" && currValue){
+           if(!isNaN(currValue)){
+            return { ...acc, name: "Other" , units: data.dataValues.units , value: parseFloat(data.dataValues[currKey])};
+           } else {
+            return { ...acc, name: "Other" , units: data.dataValues.units , value: data.dataValues[currKey]};
+           }
+          
+        }
+        return acc;
+      }, {});
     });
-    res.json({arm_summary: [{parameters: missionSummaries}]});
+
+    res.json({arm_summary: [{parameters: reducedData.filter(data => Object.keys(data).length)}]});
   } catch (error) {
     console.error("Error fetching users:", error);
     res.status(500).json({ error: "Internal server error" });
